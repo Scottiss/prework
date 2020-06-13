36bd92043cb80da10a2aff4ec9ff9eb1c6cede54
Scott Lagas <scottlagas@outlook.com>
2020-06-13 14:11:21 -0400

    prework

---
+++ PREWORK_AFH/Module-6/javascript.js
@@ -0,0 +1,14 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "200px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor='#337ab7';
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height= "150px";
+    document.getElementById("box").style.backgroundColor='#FFA500';
+});
